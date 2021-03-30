d7e672ac323e571ce9427ef3a5365511d2f8bace
geckospaniel <the2mappers@gmail.com>
2021-03-30 14:08:08 +0300

    Request permissions

--- app/src/main/java/com/mobiilikurssi/MapsActivity.kt
+++ app/src/main/java/com/mobiilikurssi/MapsActivity.kt
@@ -1,7 +1,9 @@
 package com.mobiilikurssi
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,6 +18,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val perm = arrayOf( Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION);
+
+        ActivityCompat.requestPermissions(this, perm, 1);
+
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
