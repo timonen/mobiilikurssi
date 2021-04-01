8e5a96507173f9a58e2c834ed71287a11a991aac
geckospaniel <the2mappers@gmail.com>
2021-04-01 16:04:27 +0300

    Test GPS

--- app/src/main/java/com/mobiilikurssi/MapsActivity.kt
+++ app/src/main/java/com/mobiilikurssi/MapsActivity.kt
@@ -1,9 +1,16 @@
 package com.mobiilikurssi
 
 import android.Manifest
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.TextView
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,12 +19,38 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     private lateinit var mMap: GoogleMap
+    private var locationManager : LocationManager? = null;
+
+    override fun onLocationChanged(location: Location) {
+        Log.i("test", "GPS said ${location.longitude} : ${location.latitude}");
+    }
+
+    override fun onResume() {
+        super.onResume();
+
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            Log.i("test", "Request perms");
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 2)
+        }
+
+        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1f, this);
+        Log.i("test", "Request location");
+
+        var b : TextView = findViewById(R.id.button);
+        b.setOnClickListener {
+            var l : Location? = locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Log.i("test", "GPS said last location is ${l?.longitude} : ${l?.latitude}");
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?;
+        Log.i("test", "" + locationManager);
 
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
