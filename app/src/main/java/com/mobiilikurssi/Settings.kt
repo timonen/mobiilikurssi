fb5d5d810a8452ccfde9aa783f70bfa296cca08b
valtteriviirret <viirretvaltteri@gmail.com>
2021-04-16 17:12:00 +0300

    comments

--- app/src/main/java/com/mobiilikurssi/Settings.kt
+++ app/src/main/java/com/mobiilikurssi/Settings.kt
@@ -11,16 +11,18 @@ class Settings : AppCompatActivity() {
     // either "kcal" or "kJ" depending user preferences
     var energyUnit = ""
 
-    val PREFERENCE_KEY = "PREFERENCE_KEY" // preferences stored
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_settings)
 
         // use these lines in order to get preferences
-        val sharedPreferences = getSharedPreferences(PREFERENCE_KEY, Context.MODE_PRIVATE)
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
         energyUnit = sharedPreferences.getString("energy", energyUnit).toString()
         Log.d("main", energyUnit)
+
+        // tallenna checkkaus booleanina preferensseihin
+        // when lauseella izi pizi
 
         findViewById<RadioButton>(R.id.radio_kcal).setOnClickListener() {
             energyUnit = "kcal"
@@ -36,10 +38,9 @@ class Settings : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
 
-        val sharedPreference = getSharedPreferences(PREFERENCE_KEY, Context.MODE_PRIVATE)
+        val sharedPreference = getPreferences(Context.MODE_PRIVATE)
         val editor = sharedPreference.edit()
         editor.putString("energy", energyUnit)
         editor.apply()
-
     }
 }
