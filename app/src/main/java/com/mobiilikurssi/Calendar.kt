17b3f17f5aac40e237b62872af70f60da9158f40
valtteriviirret <viirretvaltteri@gmail.com>
2021-04-20 20:57:21 +0300

    goal preferences

--- app/src/main/java/com/mobiilikurssi/Calendar.kt
+++ app/src/main/java/com/mobiilikurssi/Calendar.kt
@@ -1,10 +1,13 @@
 package com.mobiilikurssi
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Calendar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,23 @@ class Calendar : AppCompatActivity() {
 
         findViewById<Button>(R.id.button_goals).setOnClickListener {
             startActivity(Intent(this, Goal::class.java))
+
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        val pref: SharedPreferences = this.getSharedPreferences("GOAL", MODE_PRIVATE)
+
+        val getTime = pref.getString(R.array.time.toString(), "empty")
+        val getUnit = pref.getString(R.array.format.toString(), "empty")
+        val getAmount = pref.getString("amount", "1")
+        Log.d("main", getTime.toString())
+        Log.d("main", getUnit.toString())
+        Log.d("main", getAmount.toString())
+
+    }
+
+
 }
--- app/src/main/java/com/mobiilikurssi/Goal.kt
+++ app/src/main/java/com/mobiilikurssi/Goal.kt
@@ -1,35 +1,44 @@
 package com.mobiilikurssi
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 
 class Goal : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_goal)
 
         val layout1 = findViewById<LinearLayout>(R.id.time_layout)
         val layout2 = findViewById<LinearLayout>(R.id.format_layout)
+        val amount = findViewById<EditText>(R.id.text_amount)
 
+        val pref: SharedPreferences = this.getSharedPreferences("GOAL", MODE_PRIVATE)
+        val editor = pref.edit()
 
+        // making spinners
         createSpinner(R.array.time, layout1)
         createSpinner(R.array.format, layout2)
 
+        findViewById<Button>(R.id.send_button).setOnClickListener {
+            // sending preferences from EditText
+            // string now changinging to Int prolly soon
+            editor.putString("amount", amount.text.toString())
+            editor.apply()
+            }
+        }
 
-    }
 
-    private fun createSpinner(list: Int, place: LinearLayout) {
-
+    private fun createSpinner(list: Int, place: LinearLayout){
         val times = resources.getStringArray(list)
 
-        // android widget not class
+        // creating widget
         val spinner = Spinner(this)
-
         spinner.layoutParams = LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
@@ -44,20 +53,26 @@ class Goal : AppCompatActivity() {
         )
         spinner.adapter = adapter
 
+
         spinner.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
+
             override fun onItemSelected(
                     parent: AdapterView<*>,
                     view: View, position: Int, id: Long
-                ) {
-                        // code here what to do with the selected item
-                }
+            ) {
+                // setting the selected item
+                spinner.setSelection(position)
 
-                override fun onNothingSelected(parent: AdapterView<*>) {}
+                val pref: SharedPreferences = getSharedPreferences("GOAL", MODE_PRIVATE)
+                val editor = pref.edit()
 
-                }
+                // getting preferences = listname.toString()
+                editor.putString(list.toString(), spinner.selectedItem.toString())
+                editor.apply()
+            }
+            // nothing yet
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
     }
 }
-
-
-
