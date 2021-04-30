e5fbcbb5917b7df62598ca55fa3bb5f680c17f18
valtteriviirret <viirretvaltteri@gmail.com>
2021-04-30 13:44:01 +0300

    Goal.kt documentation

--- app/src/main/java/com/mobiilikurssi/Goal.kt
+++ app/src/main/java/com/mobiilikurssi/Goal.kt
@@ -12,8 +12,15 @@ import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 
+/**
+ * With this class user can set up exercise goals.
+ *
+ * @author Valtteri Viirret
+ * @version 1.0
+ */
 class Goal : AppCompatActivity() {
 
+    /** Arrays for different time and unit settings */
     private val timeArray = arrayOf<String>("päivä", "viikko", "kuukausi", "vuosi")
     private val unitArray = arrayOf<String>("kalori", "kilometri", "kilogramma")
 
@@ -25,14 +32,17 @@ class Goal : AppCompatActivity() {
         val layout1 = findViewById<LinearLayout>(R.id.time_layout)
         val layout2 = findViewById<LinearLayout>(R.id.format_layout)
         val amount = findViewById<EditText>(R.id.text_height)
-
         val pref: SharedPreferences = this.getSharedPreferences("GOAL", MODE_PRIVATE)
         val editor = pref.edit()
 
-        // making spinners
+        /**making spinners by calling function */
         createSpinner(timeArray, layout1)
         createSpinner(unitArray, layout2)
 
+        /**
+         * Setting up a "timestamp" for current goal.
+         * Formatted as a finnish date
+         */
         val currentDateTime = LocalDateTime.now()
         val calendar = java.util.Calendar.getInstance()
         val sdf = SimpleDateFormat("dd.MM.yyyy")
@@ -42,57 +52,73 @@ class Goal : AppCompatActivity() {
         val time = sdf.format(calendar.timeInMillis)
 
         findViewById<Button>(R.id.send_button).setOnClickListener {
-            // sending preferences
+
+             /** preferences for a goal*/
             editor.putString("amount", amount.text.toString())
             editor.putString("time", time)
-            // starting off fresh values
             editor.putFloat("totalkcal", 0.0f)
             editor.putFloat("totalkm", 0.0f)
             editor.apply()
+
             Toast.makeText(applicationContext, "Tavoite asetettu", Toast.LENGTH_SHORT).show()
 
+            /** intent for going back to MapsActivity */
             val intent = Intent(applicationContext, Calendar::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             intent.putExtra("EXIT", true)
             startActivity(intent)
         }
     }
-
+    /**
+     * Creating a dynamic spinner (drop-down list)
+     * @param list Array of options
+     * @param place Location for spinner
+     */
     private fun createSpinner(list: Array<String>, place: LinearLayout){
-        // creating widget
+        /** creating widget */
         val spinner = Spinner(this)
         spinner.layoutParams = LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
         )
-
-        // adding spinner to layout
+        /** adding to layout */
         place.addView(spinner)
-
+        /** making an adapter */
         val adapter = ArrayAdapter(
                 this,
                 android.R.layout.simple_spinner_item, list
         )
         spinner.adapter = adapter
 
+        /** callback to be invoked when item has been selected*/
         spinner.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
-
+            /**
+             * Function for selecting item
+             * @param parent children are list items defined in adapter
+             * @param position item's position in a list
+             */
             override fun onItemSelected(
                     parent: AdapterView<*>,
                     view: View, position: Int, id: Long
             ) {
-                // setting the selected item
+                /** setting the selected item */
                 spinner.setSelection(position)
 
                 val pref: SharedPreferences = getSharedPreferences("GOAL", MODE_PRIVATE)
                 val editor = pref.edit()
 
-                // key is the first name in the class variable list
+                /**
+                 * Key is the first item in the list used as a parameter
+                 * Value is the selected item as a string
+                 */
                 editor.putString(list[0], spinner.selectedItem.toString())
                 editor.apply()
             }
-            // nothing yet
+            /**
+             * Must be implemented because listener is abstract.
+             * Selecting nothing will already implement the first item in the list.
+             */
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
     }
