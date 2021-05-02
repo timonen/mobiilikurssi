c91c8e811527fe9c9b00859b5df3f6ca91154238
Roope Timonen <timone_n@outlook.com>
2021-05-02 16:14:09 +0300

    Update Target.kt

--- app/src/main/java/com/mobiilikurssi/Target.kt
+++ app/src/main/java/com/mobiilikurssi/Target.kt
@@ -6,39 +6,38 @@ import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.CalendarView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.math.pow
 
 
 /**
- * TODO write docs
- * Target class
- * @author
+ * This class shows off the current Goal and its progress
+ * @author Valtteri Viirret
  * @version 1.3
  */
 class Target : AppCompatActivity() {
 
     /**
-     * TODO
-     *
      * @param savedInstanceState
      */
     @SuppressLint("SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
 
         // if from Goal go back to MapsActivity
         if (intent.getBooleanExtra("EXIT", false)) {
             finish()
         }
-
-        setContentView(R.layout.activity_calendar)
-
 
         // get intent
         val km = intent.getFloatExtra("totalkm", 0.0f)
@@ -46,9 +45,11 @@ class Target : AppCompatActivity() {
         val weightset = intent.getBooleanExtra("weightset", true)
         val tracking = intent.getBooleanExtra("addProgression", false)
 
+        // define textView
         val goals = findViewById<TextView>(R.id.textview_goals)
         val myDate = findViewById<TextView>(R.id.textView_date)
         val completed = findViewById<TextView>(R.id.textView_completed)
+        val percentage = findViewById<TextView>(R.id.textView_percentage)
 
         findViewById<Button>(R.id.button_goals).setOnClickListener {
             startActivity(Intent(this, Goal::class.java))
@@ -62,35 +63,28 @@ class Target : AppCompatActivity() {
         val time = pref.getString("time", "empty")
         val editor = pref.edit()
 
+        // user preferences
+        val prefSettings = this.getSharedPreferences("SETTINGS", MODE_PRIVATE)
+        val weight = prefSettings.getString("weight", "empty")
+        val height = prefSettings.getString("height", "empty")
 
-        // input sanitation
+        // input sanitization for weight goal
         if(getUnit == "kilogramma") {
             if (getAmount != null) {
+                val w = getAmount.toInt()
                 when (getTime) {
-                    "päivä" -> {
-                        if (getAmount.toInt() > 1) {
-                            getAmount = "unreal"
-                        }
-                    }
-
-                    "viikko" -> {
-                        if (getAmount.toInt() > 4) {
-                            getAmount = "unreal"
-                        }
-                    }
-                    "kuukausi" -> {
-                        if (getAmount.toInt() > 10) {
-                            getAmount = "unreal"
-                        }
-                    }
+                    "päivä" -> if (w > 1)  getAmount = "unreal"
+                    "viikko" -> if (w > 4)  getAmount = "unreal"
+                    "kuukausi" -> if (w > 10)  getAmount = "unreal"
+                }
+                // if goalweight is below bmi 15 (anorexia)
+                val goalWeight = weight?.toInt()?.minus(w)
+                val bmi = height!!.toDouble().div(100).let { goalWeight?.div(it.pow(2)) }
+                if(bmi!! < 15.0) {
+                    getAmount = "unreal"
                 }
             }
         }
-
-        // user preferences
-        val prefSettings = this.getSharedPreferences("SETTINGS", MODE_PRIVATE)
-        val startingweight = prefSettings.getString("weight", "empty")
-
 
         /** Updating Goal preferences from MapsActivity intent  */
         if((!km.isNaN() && !kcal.isNaN()) && (km > 0 && kcal > 0)) {
@@ -105,13 +99,13 @@ class Target : AppCompatActivity() {
             }
         }
 
-        /** Get values from time */
+        // Get values from time, these time values describe time when goal was set
         var day = ""
         var month = ""
         var year = ""
         if(time != "empty") {
-            day = time?.split(".")?.get(0)?.let { removeZero(it) }.toString()
-            month = time?.split(".")?.get(1)?.let { removeZero(it) }.toString()
+            day = time?.split(".")?.get(0)?.let { RZ(it) }.toString()
+            month = time?.split(".")?.get(1)?.let { RZ(it) }.toString()
             year = time?.split(".")?.get(2).toString()
         }
 
@@ -122,8 +116,7 @@ class Target : AppCompatActivity() {
             "kilometri" -> gU = "kilometriä"
             "kilogramma" -> gU = "kilogrammaa"
         }
-
-        if(getAmount != "empty" && getTime != "empty") {
+        if(getAmount != "unreal" && getTime != "empty") {
             goals.text = "Tavoite: $getAmount $gU / $getTime"
         } else
             goals.text = "Ei uusia tavoitteita"
@@ -134,16 +127,17 @@ class Target : AppCompatActivity() {
             "kilometri" -> {
                 if (getAmount != null) {
                     val amount = pref.getFloat("totalkm", 0.0f)
-                    if (amount < getAmount.toInt()) {
+                    if (amount < getAmount.toInt()!!) {
                         completed.text = "Suoritettu %.2f".format(amount) + " km / $getAmount km"
+                        calculatePercentage(percentage, amount.toDouble(), getAmount.toDouble())
                     } else
                         completed.text = "Tavoite suoritettu!"
                 }
             }
             "kilogramma" -> {
-                if (startingweight != "empty") {
+                if (weight != "empty") {
                     if (getAmount != "unreal") {
-                        completed.text = "Tavoitepaino: ${getAmount?.toInt()?.let { startingweight?.toInt()?.minus(it) }}kg"
+                        completed.text = "Tavoitepaino: ${getAmount?.toInt()?.let { weight?.toInt()?.minus(it) }}kg"
                     } else completed.text = "Tavoitepainosi ei ole realistinen, aseta realistinen tavoite"
                 } else completed.text = "Aseta painosi asetuksissa niin näet tavoitepainosi tavoiteajan kuluttua"
             }
@@ -153,6 +147,7 @@ class Target : AppCompatActivity() {
                         val amount = pref.getFloat("totalkcal", 0.0f)
                         if (amount < getAmount.toInt()) {
                             completed.text = "Suoritettu  %.2f".format(amount) + " kcal / $getAmount kcal"
+                            calculatePercentage(percentage, amount.toDouble(), getAmount.toDouble())
                         } else
                             completed.text = "Tavoite suoritettu!"
                     } else
@@ -161,12 +156,28 @@ class Target : AppCompatActivity() {
             }
         }
 
+        // getting current date
+        val currentDateTime = LocalDateTime.now()
+        val calendar = java.util.Calendar.getInstance()
+        val sdf = SimpleDateFormat("dd.MM.yyyy")
+        calendar.set(
+            currentDateTime.year, currentDateTime.monthValue, currentDateTime.dayOfMonth
+        )
+        val currentTime = sdf.format(calendar.timeInMillis)
+        val currentDay = currentTime?.split(".")?.get(0)?.let { RZ(it) }
+        val currentMonth = currentTime?.split(".")?.get(1)?.let { RZ(it) }
+        val currentYear = currentTime?.split(".")?.get(2)?.let { RZ(it) }
+
         /** Time for goal (starting time - ending time) */
         val myG = "Tavoiteaika: "
         when(getTime) {
             "päivä" -> {
                 val newday = day.toInt().plus(1)
-                myDate.text = "$myG$day.$month.$year - $newday.$month.$year"
+
+                if(currentDay!! > day) {
+                    myDate.text = "Tavoiteaika on päättynyt"
+                } else
+                    myDate.text = "$myG${RZ(day)}.${RZ(month)}.$year - $newday.${RZ(month)}.$year"
             }
             "viikko" -> {
                 var daysInMonth = 0
@@ -174,7 +185,7 @@ class Target : AppCompatActivity() {
                 when (month) {
                     "1", "3", "5", "7", "8", "10", "12" -> daysInMonth = 31
                     "4", "6", "9", "11" -> daysInMonth = 30
-                    /** leap year check */
+                    // leap year check
                     "2" -> daysInMonth = if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) 29 else 28
                 }
                 var newday = day.toInt()
@@ -182,20 +193,32 @@ class Target : AppCompatActivity() {
                 for (i in 1..7) {
                     if (newday < daysInMonth) {
                         newday += 1
-                    } else
+                    } else {
+                        newmonth += 1
                         newday = 1
-                    newmonth += 1
-
+                    }
                 }
-                myDate.text = "$myG$day.$month.$year - $newday.$newmonth.$year"
+
+                if(currentDay!! > day && currentMonth!! > month) {
+                    myDate.text = "Tavoiteaika on päättynyt"
+                } else
+                    myDate.text = "$myG${RZ(day)}.${RZ(month)}.$year - $newday.$newmonth.$year"
             }
             "kuukausi" -> {
                 val newmonth = month.toInt().plus(1)
-                myDate.text = "$myG$day.$month.$year - $day.$newmonth.$year"
+
+                if(currentDay!! > day && currentMonth!! > month && currentYear!! > year) {
+                    myDate.text = "Tavoiteaika on päättynyt"
+                } else
+                    myDate.text = "$myG${RZ(day)}.${RZ(month)}.$year - $day.$newmonth.$year"
             }
             "vuosi" -> {
                 val newyear = year.toInt().plus(1)
-                myDate.text = "$myG$day.$month.$year - $day.$month.$newyear"
+
+                if(currentDay!! > day && currentMonth!! > month && currentYear!! > year) {
+                    myDate.text = "Tavoiteaika on päättynyt"
+                } else
+                    myDate.text = "$myG${RZ(day)}.${RZ(month)}.$year - $day.$month.$newyear"
             }
         }
     }
@@ -205,9 +228,20 @@ class Target : AppCompatActivity() {
      * @param str input string
      * @return replaced string
      */
-    private fun removeZero(str: String) : String {
+    private fun RZ(str: String) : String {
         val regex = "^0+(?!$)".toRegex()
         return regex.replace(str, "")
+    }
+
+    /**
+     * Set TextView as percentage of x, y
+     * @param t TextView element
+     * @param x
+     * @param y
+     */
+
+    private fun calculatePercentage(t: TextView, x: Double, y: Double) {
+        t.text = " %.1f".format((x / y) * 100) + "%"
     }
 
 }
